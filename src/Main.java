b9d5b28cd18f9d2dbddfc3c03e7cfebc4dd0e0e8
AndreaSciscioli <113891464+AndreaSciscioli@users.noreply.github.com>
2022-10-31 13:48:36 +0100

    stampaMenu and piattoDelGiorno methods added

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        Menu menu = new Menu("ristorante 3 menu", "PIPPO");
+        Menu menu = new Menu("ristorante 5 menu", "PIPPO");
 
         System.out.println("ANTIPASTI");
         Portata antipasto1 = new Antipasti("Antipasto:" , 8);
         Portata primoPiatto = new PrimiPiatti("spaghetti alla carbonara",13);
         Portata secondoPiatto = new SecondiPiatti("spaghetti alla carbonara",13);
-        Portata dessert = new Desser("spaghetti alla carbonara",13);
+        Portata dessert = new Desser("torta di ciliege",13);
 
         menu.addPortata(antipasto1);
         menu.addPortata(primoPiatto);
@@ -15,6 +15,7 @@ public class Main {
         menu.addPortata(dessert);
 
         menu.stampaMenu();
+        menu.piattoDelGiorno();
 
     }
 }
