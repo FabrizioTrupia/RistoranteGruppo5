975506bddf5066492400c22afb248ffeb4d46683
Alessandro Trupia <trupia.alessandro@libero.it>
2022-11-02 16:32:07 +0100

    feature\Add Javadoc

--- src/Antipasti.java
+++ src/Antipasti.java
@@ -1,5 +1,15 @@
+/**
+ * La classe che fa riferimento agli antipasti che estende la classe Portata
+ * @author Alessandro Trupia
+ * @version 1
+ */
 public class Antipasti extends Portata{
-
+    /**
+     * metodo costruttore della classe Antipasti
+     * @param type --> tipo della portata Antipasti
+     * @param name --> nome della portata Antipasti
+     * @param price --> prezzo della portata Antipasti
+     */
     public Antipasti(String type, String name, int price) {
         super(type, name, price);
     }
