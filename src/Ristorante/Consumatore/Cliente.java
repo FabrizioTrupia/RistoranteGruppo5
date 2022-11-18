6bc42e928c3802fd2cd863ce39d2c5ec4fa67866
Fabrizio <fabrziotrupia@hotmail.it>
2022-11-18 16:14:22 +0100

    feature/clienti

--- src/Ristorante/Consumatore/Cliente.java
+++ src/Ristorante/Consumatore/Cliente.java
@@ -14,16 +14,6 @@ public class Cliente {
     private String cognome;
 
     /**
-     * età cliente
-     */
-    private Integer age;
-
-    /**
-     * fascia età del cliente
-     */
-    private EnumTipoCliente type;
-
-    /**
      * gusto del cliente
      */
     private tipoGustiCliente gusti;
@@ -31,26 +21,18 @@ public class Cliente {
     /**
      * metodo costruttore cliente
      * @param cognome --> cognome del cliente
-     * @param age --> età del cliente
      * @param gusti --> gusto del cliente
      */
-    public Cliente(String cognome, Integer age, tipoGustiCliente gusti) {
+    public Cliente(String cognome, tipoGustiCliente gusti) {
         this.cognome = cognome;
-        this.age = age;
-        if (0 < age && age < 12) {
-            this.type = EnumTipoCliente.BAMBINI;
-        } else if (12 < age && age < 18) {
-            this.type = EnumTipoCliente.MINORENNE;
-        } else this.type = EnumTipoCliente.ADULTO;
         this.gusti = gusti;
     }
-
 
     /**
      * Metodo info cliente
      */
     public void infoCliente() {
-        System.out.println("Cliente: " + getCognome() + " ,anni: " + getAge() + " (Classe cliente:" + getType() + ")");
+        System.out.println("Cliente: " + getCognome() + " Gusto cliente: " + getGusti());
     }
 
 
@@ -62,21 +44,6 @@ public class Cliente {
         this.cognome = cognome;
     }
 
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public EnumTipoCliente getType() {
-        return type;
-    }
-
-    public void setType(EnumTipoCliente type) {
-        this.type = type;
-    }
 
     public tipoGustiCliente getGusti() {
         return gusti;
